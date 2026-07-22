7f6fd211aa767e1f03dab2bdea1e00853922890c
Guilhem GM <guilhem.gv.mg@gmail.com>
2026-07-22 23:19:34 +0800

    v2 projet solo basketball : function JS optimisée avec paramètre 'add'

--- JS/basketball-scorecard/index.js
+++ JS/basketball-scorecard/index.js
@@ -7,32 +7,12 @@ let guestScore = 0
 console.log(homeScoreEl)
 console.log(guestScoreEl)
 
-function add1Home() {
-    homeScore += 1
+function addHome(add) {
+    homeScore += add
     homeScoreEl.textContent = homeScore
 }
 
-function add2Home() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-}
-
-function add3Home() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-}
-
-function add1Guest() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
-}
-
-function add2Guest() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
-}
-
-function add3Guest() {
-    guestScore += 3
+function addGuest(add) {
+    guestScore += add
     guestScoreEl.textContent = guestScore
 }
