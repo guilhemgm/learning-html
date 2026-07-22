a53d15b605b2690edac54f479f18e2f2b6dcda0f
Guilhem GM <guilhem.gv.mg@gmail.com>
2026-07-22 23:33:43 +0800

    v3 basketball optimisation fonction

--- JS/basketball-scorecard/index.js
+++ JS/basketball-scorecard/index.js
@@ -1,18 +1,11 @@
-let homeScoreEl = document.getElementById("home-score")
-let guestScoreEl = document.getElementById("guest-score")
-
-let homeScore = 0
-let guestScore = 0
-
-console.log(homeScoreEl)
-console.log(guestScoreEl)
-
-function addHome(add) {
-    homeScore += add
-    homeScoreEl.textContent = homeScore
+let scoreEl = {
+    home: document.getElementById("home-score"),
+    guest: document.getElementById("guest-score")
 }
 
-function addGuest(add) {
-    guestScore += add
-    guestScoreEl.textContent = guestScore
+let score = {home: 0, guest: 0}
+
+function addScore(team, add) {
+    score[team] += add
+    scoreEl[team].textContent = score[team]
 }
